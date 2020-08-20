ff862afb3609d7f4a8e28c8fef647ce17ced1127
SANA BEN <sana.bengannoune@gmail.com>
2020-08-20 12:50:45 +0100

    login done

---
+++ dist/js/script.js
@@ -0,0 +1,21 @@
+
+// click btn menu humbrger:::::::::::::
+
+const btn = document.getElementById('btn-menu');
+const menu = document.getElementById('menu');
+const nav = document.getElementById('nav');
+
+
+btn.addEventListener('click',openMenu);
+nav.addEventListener('click',closeMenu);
+
+
+function openMenu(){
+    
+    menu.style.display='block';  
+}
+
+function closeMenu(){
+    
+    menu.style.display='none';  
+}
